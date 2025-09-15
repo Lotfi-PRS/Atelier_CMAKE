bd5eb2e6c6a55a8869ccd22567be2f19e71c8a32
Lotfi-PRS <86322949+Lotfi-PRS@users.noreply.github.com>
2025-09-15 14:49:16 +0200

    Update calculator.c

--- src/lib/calculator.c
+++ src/lib/calculator.c
@@ -15,4 +15,11 @@ double _mul(double a, double b) {
 double _div(double a, double b) {
 	return a/b;
 }
-	
+double _car(double a, double b) {
+	double res = 1;
+	while(b > 0)
+	{
+		res = res * a;
+		b = b - 1;
+	}
+	return res;
